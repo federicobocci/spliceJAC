fb2184aa717faf82a040767c03713e625c173036
Federico Bocci <fede.bocci91@gmail.com>
2022-09-15 09:53:40 -0700

    add read the docs configuration-5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,8 +31,8 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "myst_parser",
     "sphinx.ext.duration",
+    "myst_parser",
     "sphinx.ext.autosectionlabel",
     "sphinx.ext.autodoc",
     "nbsphinx",
